e4bd61caf7e90be21a06b4f667a1afaa20934c7e
Zak Croft <zak.croft@mokriya.com>
2020-11-15 16:32:13 +0000

    categories

--- screens/CategoryMeals.js
+++ screens/CategoryMeals.js
@@ -1,39 +1,53 @@
 import React from "react";
-import { View, Text, StyleSheet, Button, Platform } from "react-native";
+import { View, Text, FlatList, StyleSheet } from "react-native";
 
-import { CATEGORIES } from "../data/dummy-data";
-import Colors from "../constants/Colors";
-import { CategoriesScreen } from "./CategoriesScreen";
+import { CATEGORIES, MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
 
 export const CategoryMealScreen = props => {
-  const catId = props.navigation.getParam("categoryId");
-  const selectedCategory = CATEGORIES.find(c => c.id === catId);
-
-  return (
-    <View style={styles.screen}>
-      <Text>The Category Meal Screen</Text>
-      <Text>{selectedCategory.title}</Text>
-      <Button
-        title="Go to meal details"
-        onPress={() => {
+  const renderMealItem = itemData => {
+    return (
+      <MealItem
+        title={itemData.item.title}
+        image={itemData.item.imageUrl}
+        duration={itemData.item.duration}
+        complexity={itemData.item.complexity}
+        affordability={itemData.item.affordability}
+        onSelectMeal={() => {
           props.navigation.navigate({
-            routeName: "MealDetailsScreen"
+            routeName: "MealDetail",
+            params: {
+              mealId: itemData.item.id
+            }
           });
         }}
       />
-      <Button
-        title="Go back"
-        onPress={() => {
-          props.navigation.pop();
-        }}
+    );
+  };
+
+  const catId = props.navigation.getParam("categoryId");
+
+  const displayedMeals = MEALS.filter(
+    meal => meal.categoryIds.indexOf(catId) >= 0
+  );
+
+  return (
+    <View style={styles.screen}>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item, index) => item.id}
+        renderItem={renderMealItem}
+        style={{ width: "100%" }}
       />
     </View>
   );
 };
 
-CategoryMealScreen.navigationOptions = navigationdata => {
-  const catId = navigationdata.navigation.getParam("categoryId");
-  const selectedCategory = CATEGORIES.find(c => c.id === catId);
+CategoryMealScreen.navigationOptions = navigationData => {
+  const catId = navigationData.navigation.getParam("categoryId");
+
+  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+
   return {
     headerTitle: selectedCategory.title
   };
@@ -43,6 +57,7 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
+    padding: 15
   }
 });
